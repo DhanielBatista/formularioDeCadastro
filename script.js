5c3c710a26cc4f2848dac621799fe80b1fae82af
Dhaniel Batista <dhanielbatista@gmail.com>
2021-03-11 23:11:41 -0300

    mudança no texto de cadastro concluido

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function ConfirmarCadastro(){
     let Email = document.getElementById('Email')
     let Usuario = document.getElementById('NomeUsuario')
     let Senha = document.getElementById('Senha')
-
-
+    let formularioCadastrado = document.querySelector('div#FormularioCadastrado')
+    let Questionario = document.querySelector('div#Questionario')
+    
     if( NomeCompleto.value.length == 0 || Endereco.value.length == 0 || Email.value.length == 0|| Usuario.value.length == 0|| Senha.value.length == 0 ){
         window.alert('Preencha todos os dados')
     }else{
-        window.alert('Todos os dados preenchidos')
+        Questionario.style.display = 'none'
+        formularioCadastrado.innerHTML = `Formulario Cadastrado com Sucesso`
+        
     }
 }
